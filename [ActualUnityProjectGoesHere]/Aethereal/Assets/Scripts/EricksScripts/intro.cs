e4d5161b2279dbd9b77605f04e26c909a1ef12f4
Jorge <jsjjbuckley@ufl.edu>
2020-03-23 22:28:13 -0400

    Added Erick's Heitzman Scripts for all to use

---
+++ [ActualUnityProjectGoesHere]/Aethereal/Assets/Scripts/EricksScripts/intro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class intro : MonoBehaviour
+{
+    private void Start()
+    {
+        loadnext();
+    }
+    private void loadnext()
+    {
+        StartCoroutine(ActivationRoutine());
+    }
+    private IEnumerator ActivationRoutine()
+    {
+
+        //wait a second
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(2);
+
+
+    }
+}
