3d0909a803a03d0398db3c9708a56f2516b3cab1
Jorge <jsjjbuckley@ufl.edu>
2020-03-27 21:44:06 -0400

    Added all levels of game to one unity project

---
+++ [ActualUnityProjectGoesHere]/Aethereal/Assets/OurAssets/EarthlevelAssets/UpAndDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpAndDown : MonoBehaviour
+{
+    private bool upLimit = false;
+    private bool downLimit = true;
+    void Update()
+    {
+        if (gameObject.transform.position.y <= -0.95)
+        {
+            downLimit = true;
+            upLimit = false;
+        }
+        else if (gameObject.transform.position.y >= 0.95)
+        {
+            upLimit = true;
+            downLimit = false;
+        }
+
+        if (upLimit)
+            gameObject.transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+        else if (downLimit)
+            gameObject.transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+
+    }
+}
---
+++ [ActualUnityProjectGoesHere]/Aethereal/Assets/Scripts/EricksScripts/Respawner.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Respawner : MonoBehaviour
+{
+    
+    private GameObject respawn;
+    public int playerScore;
+    //STORING PLATFORMS
+    List<Vector3> platformPos = new List<Vector3>();
+
+
+    [Tooltip("The score value of a coin or pickup.")]
+    public int coinValue = 10;
+
+    public Text scoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //STORING PLATFORMS
+        StorePlatforms();
+
+        respawn = GameObject.FindGameObjectWithTag("Respawn");
+        playerScore = 0;
+        //scoreText.text = playerScore.ToString("D4");
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Death"))
+        {
+            Respawn();
+        }
+        else if (collision.CompareTag("Coin"))
+        {
+            AddPoints(10);
+            Destroy(collision.gameObject);
+        }
+        else if (collision.CompareTag("Finish"))
+        {
+            Time.timeScale = 0;
+        }
+
+    }
+
+    public void Respawn()
+    {
+        //RESETTING PLATFORMS
+        ResetPlatforms();
+        gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        gameObject.transform.position = respawn.transform.position;
+        
+    }
+    //RESETTING PLATFORMS
+    public void StorePlatforms()
+    {
+        List<GameObject> objs = new List<GameObject>();
+        objs.AddRange(GameObject.FindGameObjectsWithTag("Ground"));
+        foreach(GameObject platform in objs)
+        {
+            platformPos.Add(platform.transform.position);
+        }
+    }
+    public void ResetPlatforms()
+    {
+        List<GameObject> objs = new List<GameObject>();
+        objs.AddRange(GameObject.FindGameObjectsWithTag("Ground"));
+        int i = 0;
+        foreach (GameObject platform in objs)
+        {           
+            platform.transform.position = platformPos[i];
+            platform.GetComponent<Rigidbody2D>().isKinematic = true;
+            platform.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+            platform.GetComponent<Rigidbody2D>().angularVelocity = 0f;
+            i++;
+        }
+    }
+
+    public int GetScore()
+    {
+        return playerScore;
+    }
+
+    public void AddPoints(int amount)
+    {
+        playerScore += amount;
+        scoreText.text = playerScore.ToString();
+    }
+
+}
+
